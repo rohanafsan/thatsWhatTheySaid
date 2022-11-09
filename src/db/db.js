e9c64059b902d6bd158cca1083d2beba65b806a8
Rohan Afsan <notrohanaf@gmail.com>
2022-11-09 16:31:02 -0500

    update db

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,6 @@ const Pool = require ("pg").Pool;
 
 const pool = new Pool({
     user: "dundermifflin",
-    host: "localhost",
     database: "officedb",
     password: "keke11EKEK,",
     port: "5432",
