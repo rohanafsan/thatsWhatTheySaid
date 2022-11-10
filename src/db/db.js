c2359afed990f693840af0439f07916c946913ad
Rohan Afsan <notrohanaf@gmail.com>
2022-11-09 19:51:06 -0500

    change db variables

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,11 @@
 const Pool = require ("pg").Pool;
 
 const pool = new Pool({
-    user: "dundermifflin",
-    database: "officedb",
-    password: "keke11EKEK,",
-    port: "5432",
+    user: "postgres",
+    database: "railway",
+    host: "containers-us-west-107.railway.app",
+    password: "36HJkwMouocNZ2fTCmOH",
+    port: "7426",
 });
 
 module.exports = pool;
